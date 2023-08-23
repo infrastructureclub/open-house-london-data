4d47fddc2bffcef87b561a064e2fa01b13c9b9ec
Jonty Wareing <jonty@Jontys-MacBook-Pro.local>
2023-08-23 17:43:16 +0100

    Oh come on, these rate limits are absurd

--- scrape-openhouse.py
+++ scrape-openhouse.py
@@ -342,5 +342,5 @@ for building in buildings:
             )
         )
 
-    # 4s appears to avoid the rate limiting, but let's give ourselves some headroom
-    time.sleep(4)
+    # 7s appears to avoid the rate limiting, but let's give ourselves some headroom
+    time.sleep(8)
