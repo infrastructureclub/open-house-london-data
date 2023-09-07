0da596089c04dc496d96606d0fc87afe93060f17
Jonty Wareing <jonty@jonty.co.uk>
2023-09-07 20:34:33 +0100

    Add referer because otherwise events are hidden
    Unless you're in the UK, in which case they're not. Amazing work.

--- scrape-openhouse.py
+++ scrape-openhouse.py
@@ -15,6 +15,7 @@ headers={
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
     'Accept-Language': 'en-GB,en;q=0.9',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Referer': 'https://programme.openhouse.org.uk/',
 }
 
 cookies = {}
